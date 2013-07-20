3340ae65176cef68167bf2a1aff0166c9e6319b1
Olin Atkinson <atkinsonos@familysearch.org>
2013-07-19 22:35:20 -0600

    Fixed .splunkrc file parsing to handle blank lines.

--- app/Command.scala
+++ app/Command.scala
@@ -14,10 +14,8 @@ import scala.collection.JavaConversions._
 case class Command(appName: String = "App") {
   val defaultValues = HashMap[String, Object]("scheme" -> "https", "host" -> "localhost", "port" -> new Integer(8089))
   val rcLinesUnFiltered = Source.fromFile(Properties.userHome + File.separator + ".splunkrc").getLines().toList
-  val splunkrc = rcLinesUnFiltered.filterNot( _.startsWith("#")).map(_.split("=")).map(e => (e(0), e(1))) toMap
+  val splunkrc = rcLinesUnFiltered.filterNot( l => l.isEmpty || l.startsWith("#")).map(_.split("=")).map(e => (e(0), e(1))).toMap
   val opts: Map[String, Object] = defaultValues ++ splunkrc
-
-
 }
 
 object Command {
